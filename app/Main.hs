1f6b4e4dfc41a0ca570a6d0c1344f837562722a9
Jaan Van Gils <j.j.g.vangils@students.uu.nl>
2021-09-22 22:06:45 +0200

    Make POC for wlp calculator

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Lib
+import POC
 
-main :: IO ()
-main = someFunc
+--main :: IO () --Removed because POC.run has different type
+main = do r <- run
+          putStrLn (show r)
