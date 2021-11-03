e7ea3414a57d78683bf3cbf2f525fd4bac13fcc3
bhaak-git <17254355+bhaak-git@users.noreply.github.com>
2021-11-03 13:54:00 +0100

    Run benchmarks for all N in [2..10]

--- bench/Spec.hs
+++ bench/Spec.hs
@@ -4,24 +4,23 @@ import Criterion.Main (bench, defaultMain, nfIO)
 import GCLParser.GCLDatatype
 import GCLParser.Parser (parseGCLfile)
 import System.Directory (listDirectory)
-import Verifier (arguments, verifyProgram)
+import Verifier (verifyProgram)
 
 modProgram :: Either a Program -> Int -> Either a Program
-modProgram (Left program) n = Left program
+modProgram (Left error) n = Left error
 modProgram (Right (Program name input output stmt)) n = do
   let inp = VarDeclaration "" (PType PTInt)
   let seq = Seq (Assign "N" (LitI n)) stmt
   Right (Program name (inp : input) output seq)
 
-run :: [Char] -> IO ()
-run file = do
+run :: [Char] -> Int -> IO ()
+run file n = do
   program <- parseGCLfile file
-  -- N = 2 and K = 10
-  -- TODO: ForAll N in [2..10] use deep enough K
-  verifyProgram (modProgram program 2) (10, file, False, False)
+  -- TODO: use deep enough K (is now always 1)
+  verifyProgram (modProgram program n) (1, file, False, False)
 
 main :: IO ()
 main = do
   let dir = "bench/input/"
   files <- listDirectory dir
-  defaultMain [bench file $ nfIO (run (dir ++ file)) | file <- files]
+  defaultMain [bench file $ nfIO (run (dir ++ file) n) | file <- files, n <- [2 .. 10]]
