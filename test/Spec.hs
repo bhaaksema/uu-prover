f9c47e68fea1b9064abc42fa91b290c24d22642f
bhaaksema <b.haaksema@students.uu.nl>
2021-11-09 21:41:32 +0100

    Give the test suite an overdue update

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,15 @@
 module Main where
 
+import Control.Monad (void)
 import GCLParser.Parser (parseGCLfile)
 import System.Directory (listDirectory)
-import Verifier (arguments, verifyProgram)
+import Verifier (verifyProgram)
 
 run :: [Char] -> IO ()
 run file = do
   let parsedArgs = (10, file, False, False)
   program <- parseGCLfile file
-  verifyProgram program parsedArgs
+  void (verifyProgram program parsedArgs)
 
 main :: IO ()
 main = do
