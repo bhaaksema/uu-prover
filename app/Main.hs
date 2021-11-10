bb224f9878741c814e22308f9088922dce911858
bhaaksema <b.haaksema@students.uu.nl>
2021-11-10 03:09:13 +0100

    Create README and minor changes

--- app/Main.hs
+++ app/Main.hs
@@ -5,27 +5,26 @@ import GCLParser.Parser (parseGCLfile)
 import System.Environment (getArgs)
 import Verifier (verifyProgram)
 
--- The following functions will run the 'main' program and output the required information
--- main loads the file and puts the ParseResult Program through the following functions
-arguments :: [[Char]] -> (Int, [Char], Bool, Bool)
-arguments [] = (10, "test/input/reverse.gcl", False, False)
-arguments ("-K" : arg : xs) = (read arg, a2, a3, a4)
+parseArgs :: [[Char]] -> (Int, [Char], Bool, Bool)
+parseArgs [] = (10, "test/input/reverse.gcl", False, False)
+parseArgs ("-K" : arg : args) = (read arg, a2, a3, a4)
   where
-    (_, a2, a3, a4) = arguments xs
-arguments ("-file" : arg : xs) = (a1, arg, a3, a4)
+    (_, a2, a3, a4) = parseArgs args
+parseArgs ("-file" : arg : args) = (a1, arg, a3, a4)
   where
-    (a1, _, a3, a4) = arguments xs
-arguments ("-wlp" : xs) = (a1, a2, True, a4)
+    (a1, _, a3, a4) = parseArgs args
+parseArgs ("-wlp" : args) = (a1, a2, True, a4)
   where
-    (a1, a2, _, a4) = arguments xs
-arguments ("-path" : xs) = (a1, a2, a3, True)
+    (a1, a2, _, a4) = parseArgs args
+parseArgs ("-path" : args) = (a1, a2, a3, True)
   where
-    (a1, a2, a3, _) = arguments xs
-arguments (x : xs) = arguments xs
+    (a1, a2, a3, _) = parseArgs args
+parseArgs (arg : args) = parseArgs args
 
+-- loads the file and puts the ParseResult Program through the functions in Verifier
 main :: IO ()
 main = do
   args <- getArgs
-  let parsedArgs@(_, file, _, _) = arguments args
+  let parsedArgs@(_, file, _, _) = parseArgs args
   program <- parseGCLfile file
   void (verifyProgram program parsedArgs)
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,9 +5,9 @@ import GCLParser.Parser (parseGCLfile)
 import System.Directory (listDirectory)
 import Verifier (verifyProgram)
 
-run :: [Char] -> IO ()
-run file = do
-  let parsedArgs = (10, file, False, False)
+run :: Int -> [Char] -> IO ()
+run k file = do
+  let parsedArgs = (k, file, False, False)
   program <- parseGCLfile file
   void (verifyProgram program parsedArgs)
 
@@ -15,4 +15,4 @@ main :: IO ()
 main = do
   let dir = "test/input/"
   files <- listDirectory dir
-  foldr (>>) (print "") [run (dir ++ file) | file <- files]
+  foldr (>>) (print "") [run k (dir ++ file) | file <- files, k <- [0 .. 20]]
